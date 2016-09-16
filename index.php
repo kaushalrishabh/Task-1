39f645553c9c5f4fc7ea019cb0d52c414083d09c
Rishabh Kaushal <kaushal.rishabh4@gmail.com>
2016-09-16 19:24:25 +0530

    New Version

--- index.php
+++ index.php
@@ -24,17 +24,28 @@
 								<div class = "form-group">
 									<input type = "submit" value ="LOGIN" class ="btn btn-primary form-control"> 
 								</div>
+								<div class="form-group">
+										<div class ="checkbox">
+											<label>
+												<input type="checkbox" class ="form-check-input"> Remember Me	
+											</label>
+										</div>
+								</div>
 								<div class= "form-group">
 									<label> <a href="forget_password.php"> Forget Password ? </a> </label>
 								</div>
 								<div>
 									<label>	Need a Account! </label>
-									<a href ="SignUp.php" class="btn btn-primary">Sign Up	</a>
+									<a href="SignUp.php" class="btn btn-primary form-control "> Sign Up </a>	
 								</div>
 						</form>
 					</div>
-					<div class = "col-md-4"> </div> 
+					<div class = "col-md-4"> 
+					</div> 
 				</div>
 			</div>	
+			<footer class="text"> Risk Corp : All Rights Reserved <sup> &reg </sup>  
+			</footer>
+			
 	</body>
 </html>
