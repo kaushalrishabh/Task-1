b6dfeb47901bb12ca1404af812f0e92458052b89
Rishabh Kaushal <kaushal.rishabh4@gmail.com>
2016-09-16 17:30:43 +0530

    LOGIN

--- index.php
+++ index.php
@@ -1,11 +1,33 @@
 <!DOCTYPE HTML>
 <html>
-<head>
-	<title> 	Oh Yeah!	</title>
-</head>
-<body>
-	<p> HELLO WORLD </p>
-	<p align ="center"> NEW LINE </p>
-	<p> THIRD COMMIT </p>
-</body>
+	<head>
+			<title> Login :: Log in to your Account	</title>
+			<link rel = "stylesheet" type = "text/css" href = "Bootstrap/css/bootstrap.css">
+	</head>
+	<body>	
+		<div class = "container">
+			<div class = "jumbotron">
+				<h1 class = "text-centre"> LOGIN </h1>
+			</div>			
+				<div class = "row">
+					<div class = "col-md-4"> </div> 
+					<div class = "col-md-4"> 
+						<form>
+								<div class = "form-group">
+									<label> Email Address </label>
+									<input type = "email" class ="form-control" >
+								</div>
+								<div class = "form-group">
+									<label> Password </label>
+									<input type = "password" class ="form-control" >
+								</div>
+								<div class = "form-group">
+									<input type = "submit" value ="LOGIN" class ="btn btn-primary form-control"> 
+								</div>
+						</form>
+					</div>
+					<div class = "col-md-4"> </div> 
+				</div>
+			</div>	
+	</body>
 </html>
---
+++ login.php
@@ -0,0 +1,11 @@
+<!DOCTYPE HTML>
+<html>
+	<head>
+		<title> Login :: Log in to your Account	</title>
+	</head>
+
+	<body>	
+
+
+	</body>
+</html>
